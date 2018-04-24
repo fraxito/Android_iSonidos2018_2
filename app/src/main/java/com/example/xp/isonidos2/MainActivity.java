e4b2cd49a4b7ffc982b4e3eb4b4b182e2662e08f
Jorge Cisneros <fraxito@gmail.com>
2018-04-24 16:15:06 +0200

    Añadido el video de "Siiii " de los simpsons
    He cambiado los colores de fondo de los botones, si es un audio es azul y si es un video es naranja
    
    He cambiado el icono de la app
    
    He cambiado varios nombres de sonidos para que queden ordenados

--- app/src/main/java/com/example/xp/isonidos2/MainActivity.java
+++ app/src/main/java/com/example/xp/isonidos2/MainActivity.java
@@ -164,6 +164,11 @@ public class MainActivity extends AppCompatActivity {
         b.setBackgroundColor(Color.BLUE);
         b.setAllCaps(true); //todas las letras del botón en minúscula
         int id = this.getResources().getIdentifier(_listaCanciones[i].getName(), "raw", this.getPackageName());
+        String nombreLargo =  _listaCanciones[i].getName();
+
+        if (nombreLargo!= null && nombreLargo.substring(0,2).contains("v_")) {
+            b.setBackgroundColor(Color.rgb(255,140,0));
+        }
         b.setTag(id);
         b.setId(i+50);
         b.setOnClickListener(new View.OnClickListener() {
